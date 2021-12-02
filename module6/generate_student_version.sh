8eb15d3d98d13f4817c9bb03c23c255003d70a71
Yasmine Bassil <53958017+yasminebassil@users.noreply.github.com>
2021-12-02 09:13:09 -0500

    delete m6 dir

--- module6/generate_student_version.sh
+++
@@ -1,3 +0,0 @@
-# Ignore this file; only used to generate a 'student version' of the notebook
-nbgrader generate_assignment -f --assignment python4mri --notebook python4mri --course-dir .
-cp release/python4mri/* .
